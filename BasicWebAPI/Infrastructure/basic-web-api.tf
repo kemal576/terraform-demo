5767b32d46320ca0e770d7aebee2199ec6852eba
kemal.sahin <kemal.sahin@hangipara.com.tr>
2024-09-26 03:56:25 +0300

    allow 8080 port

--- BasicWebAPI/Infrastructure/basic-web-api.tf
+++ BasicWebAPI/Infrastructure/basic-web-api.tf
@@ -29,27 +29,28 @@ resource "aws_instance" "app_server" {
 
 resource "aws_security_group" "app_server_sg" {
   name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP"
+  description = "Allow SSH and HTTP with 8080 port"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SSH için tüm IP'lere izin ver
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # HTTP için tüm IP'lere izin ver
+    description      = "Allow traffic on port 8080"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Dışa çıkış trafiğine tüm portlardan izin ver
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
